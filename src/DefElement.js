27a7de644c378c012c368b23bc6d75b496c5bab1
urkkiz225 <urkkiz225@gmail.com>
2023-04-25 17:25:12 +0300

    enviroment deployment test

--- src/DefElement.js
+++ src/DefElement.js
@@ -5,8 +5,7 @@ function DefElement(args){
     <div class="element">
       <img src={args.image} width={762} height={432} alt='kesäSimulaattori'/>
       <h4 style={{color:"red"}}>{args.text}</h4>
-      <a href="/#" onclick={args.link} class="btn btn-dark"> Link to {args.text} GitHub repo</a>
-      <button onclick={args.link} id="myButton" class="btn btn-dark" >{args.text}</button>
+      <a href="/#" onclick={args.link} class="btn btn-dark"> {args.text}</a>
     </div>
   );
 }
