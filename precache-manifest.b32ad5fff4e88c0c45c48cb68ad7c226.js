3ac7bebb94ed369d4350ee9ce8721b5905ca85f0
urkkiz225 <urkkiz225@gmail.com>
2023-04-25 17:50:55 +0300

    Updates

---
+++ precache-manifest.b32ad5fff4e88c0c45c48cb68ad7c226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1442d789f4214e2c20c4",
+    "url": "./static/css/main.3d8bc17e.chunk.css"
+  },
+  {
+    "revision": "1442d789f4214e2c20c4",
+    "url": "./static/js/main.1442d789.chunk.js"
+  },
+  {
+    "revision": "6ef926b70523b171d0f4",
+    "url": "./static/js/1.6ef926b7.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "596a9cf4f6dbd688743e85657fc3b951",
+    "url": "./static/media/chickenWareShowCase.596a9cf4.png"
+  },
+  {
+    "revision": "b709163fdfa43eeadb71848cf64be3a7",
+    "url": "./static/media/kesasim.b709163f.png"
+  },
+  {
+    "revision": "6f60944909c7b91bfe48965de619e083",
+    "url": "./index.html"
+  }
+];
