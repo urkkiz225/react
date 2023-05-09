b15ae0faa7db2776c2bec0d8b714a4708dc80501
urkkiz225 <urkkiz225@gmail.com>
2023-05-09 17:43:09 +0300

    problem fix for homepage

---
+++ src/pages/CV.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+import Header from "./UtilClasses/Header.js"
+
+const HomePage = () => {
+  return(
+<div>
+  <Header/>
+ </div>);
+};
+export default HomePage;
